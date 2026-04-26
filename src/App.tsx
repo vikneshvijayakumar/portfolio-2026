d6b3be58bee0c8df145bf29d2021a48e7f990a83
Viknesh <viknesh@Vikneshs-MacBook-Pro.local>
2026-04-26 13:54:42 +0530

    perf: load GA on first interaction, prioritize LCP badge image

--- src/App.tsx
+++ src/App.tsx
@@ -1186,7 +1186,7 @@ const BadgesCluster = memo(function BadgesCluster({
           scale: { type: "spring", stiffness: 300, damping: 25, delay: 0.05 }
         }}
       >
-        <img src={upworkBadge} alt="Upwork Top Rated" className="badge-image badge-image--upwork" width={200} height={56} draggable="false" loading="lazy" decoding="async" />
+        <img src={upworkBadge} alt="Upwork Top Rated" className="badge-image badge-image--upwork" width={200} height={56} draggable="false" decoding="async" fetchPriority="high" />
         <span className="badge-tooltip">View Profile</span>
       </motion.a>
       <motion.a
@@ -1198,7 +1198,7 @@ const BadgesCluster = memo(function BadgesCluster({
           scale: { type: "spring", stiffness: 300, damping: 25, delay: 0.1 }
         }}
       >
-        <img src={googleUxBadge} alt="Google UX Design Certificate" className="badge-image badge-image--google" width={200} height={200} draggable="false" loading="lazy" decoding="async" />
+        <img src={googleUxBadge} alt="Google UX Design Certificate" className="badge-image badge-image--google" width={200} height={200} draggable="false" decoding="async" />
         <span className="badge-tooltip">See Certificate</span>
       </motion.a>
     </div>
